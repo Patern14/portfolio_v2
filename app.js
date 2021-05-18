2f75fdbe45b14bb99eeb1f40142adc0d67fcc4b1
Patern14 <paternmalle@gmail.com>
2021-05-18 13:39:18 +0100

    Menu burger.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("App.js CONNECTED")
+
+const navToggle = document.querySelector('.nav_toggle');
+const navLinks = document.querySelectorAll('.nav_link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav_open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav_open');
+    })
+});
